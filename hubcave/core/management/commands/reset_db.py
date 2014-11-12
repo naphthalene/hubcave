d1ed9d8ee7034b07987dc6c51f159a35d518d708
Pasha Sadikov <pashalab@gmail.com>
2014-11-12 15:28:56 -0500

    fixing settings refs

--- hubcave/core/management/commands/reset_db.py
+++ hubcave/core/management/commands/reset_db.py
@@ -17,29 +17,34 @@ class Command(BaseCommand):
 
         print settings
         engine = settings.DATABASES['default']['ENGINE']
+        name = settings.DATABASES['default']['NAME']
+        user = settings.DATABASES['default']['USER']
+        passwd = settings.DATABASES['default']['PASSWORD']
+        host = settings.DATABASE['default']['HOST']
+        port = settings.DATABASE['default']['PORT']
     
         if engine == 'django.db.backends.sqlite3':
             import os
             try:
                 logging.info("Unlinking sqlite3 database")
-                os.unlink(settings.DATABASE_NAME)
+                os.unlink(name)
             except OSError:
                 pass
         elif engine == 'django.db.backends.mysql':
             import MySQLdb as Database
             kwargs = {
-                'user': settings.DATABASE_USER,
-                'passwd': settings.DATABASE_PASSWORD,
+                'user': user,
+                'passwd': ,
             }
-            if settings.DATABASE_HOST.startswith('/'):
-                kwargs['unix_socket'] = settings.DATABASE_HOST
+            if host.startswith('/'):
+                kwargs['unix_socket'] = host
             else:
-                kwargs['host'] = settings.DATABASE_HOST
-            if settings.DATABASE_PORT:
-                kwargs['port'] = int(settings.DATABASE_PORT)
+                kwargs['host'] = host
+            if port:
+                kwargs['port'] = int(port)
             connection = Database.connect(**kwargs)
-            drop_query = 'DROP DATABASE IF EXISTS %s' % settings.DATABASE_NAME
-            create_query = 'CREATE DATABASE %s' % settings.DATABASE_NAME
+            drop_query = 'DROP DATABASE IF EXISTS %s' % name
+            create_query = 'CREATE DATABASE %s' % name
             logging.info('Executing... "' + drop_query + '"')
             connection.query(drop_query)
             logging.info('Executing... "' + create_query + '"')
@@ -47,21 +52,21 @@ class Command(BaseCommand):
         elif engine == 'django.db.backends.postgresql_psycopg2':
             import psycopg2 as Database
     
-            if settings.DATABASE_NAME == '':
+            if name == '':
                 from django.core.exceptions import ImproperlyConfigured
                 raise ImproperlyConfigured, "You need to specify DATABASE_NAME in your Django settings file."
-            if settings.DATABASE_USER:
-                conn_string = "user=%s" % (settings.DATABASE_USER)
-            if settings.DATABASE_PASSWORD:
-                conn_string += " password='%s'" % settings.DATABASE_PASSWORD
-            if settings.DATABASE_HOST:
-                conn_string += " host=%s" % settings.DATABASE_HOST
-            if settings.DATABASE_PORT:
-                conn_string += " port=%s" % settings.DATABASE_PORT
+            if user:
+                conn_string = "user=%s" % (user)
+            if passwd:
+                conn_string += " password='%s'" % passwd
+            if host:
+                conn_string += " host=%s" % host
+            if port:
+                conn_string += " port=%s" % port
             connection = Database.connect(conn_string)
             connection.set_isolation_level(0) #autocommit false
             cursor = connection.cursor()
-            drop_query = 'DROP DATABASE %s' % settings.DATABASE_NAME
+            drop_query = 'DROP DATABASE %s' % name
             logging.info('Executing... "' + drop_query + '"')
     
             try:
@@ -75,7 +80,7 @@ CREATE DATABASE %s
     WITH OWNER = %s
         ENCODING = 'UTF8'
         TABLESPACE = pg_default;
-""" % (settings.DATABASE_NAME, settings.DATABASE_USER))
+""" % (name, user))
             logging.info('Executing... "' + create_query + '"')
             cursor.execute(create_query)
     
